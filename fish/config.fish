2c0757bab9298cd6c4d33958730ac710b0ef6373
izco42 <izcoatlramirezq@gmail.com>
2025-11-22 22:59:33 -0600

    add fastfetch

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     # Reemplazar ls con lsd
     alias ls='lsd'
+    fastfetch --kitty-icat ~/.config/fastfetch/pic/astro.png --logo-width 30
+
 end
 starship init fish | source
